af211bb59a1225e49d11bbe72251939e739cd145
João Pedro Mendes <joao99.jp@gmail.com>
2021-03-17 12:08:20 -0300

    Desafios em C#

---
+++ Desafios/1 - Múltiplos de 3/MultiplosDeTres/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+ Utilize um laço do tipo for para imprimir todos os múltiplos de 3, entre 1 e 100.
+
+Dica: Existem duas maneiras tradicionais de resolver este problema. Uma delas consiste em fazer o for, e utilizar numero % 3 para descobrir o resto da divisão de um número por 3 (o operador % se chama de módulo). Se o resto for zero, ele é divisível por 3.
+ */
+using System;
+
+namespace MultiplosDeTres
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            for (int numero = 3; numero <= 100; numero += 3)
+            {                
+                Console.WriteLine(numero);                               
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Desafios/2 - Fatorial/Fatorial/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+Agora crie uma nova classe, escreva um for que inicie uma variável n (número atual) como 1 e fatorial (resultado total) como 1. Faça seu laço entre 1 a 10 e calcule o resultado!
+*/
+
+using System;
+
+namespace Fatorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int fatorial = 1;            
+
+            for (int n = 1; n <= 10; n++)
+            {
+                fatorial *= n;
+                Console.WriteLine($"Fatorial de {n} é {fatorial}!");                                
+            }
+            Console.ReadLine();
+        }
+    }
+}
